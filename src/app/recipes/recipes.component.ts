0fbf111526b7dbecac394aeddf30d12cc31ecbaf
Ben Broadstone <bbroadstone@sharedsvcs.com>
2020-07-12 15:51:48 -0400

    feature: component communication using Subjects through service layers

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-
-import {Recipe} from './recipe.model';
 import {RecipeService} from './recipe.service';
 
 @Component({
@@ -10,19 +8,13 @@ import {RecipeService} from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {
+  constructor() {
+
   }
 
-  // tslint:disable-next-line:typedef
-  ngOnInit() {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      );
+  ngOnInit(): void {
+
   }
 
 }
